5c09be2e0669b4b0d7184011add1289427ff22d7
aparna4scholas <aparna4scholas@gmail.com>
2020-05-27 15:07:39 -0500

    modified

--- stopwatch/src/components/Stopwatch.js
+++ stopwatch/src/components/Stopwatch.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
  class Stopwatch extends Component {
+     constructor(props){
+         super(props);
+         this.start = this.start.bind(this);
+         this.reset = this.reset.bind(this);
+         this.pause = this.pause.bind(this);
+
+     }
     render() {
         return (
             <div>
-                
+                <h1></h1>
+                <button onClick={this.start} >
+                    Start
+                </button>
             </div>
         )
     }
