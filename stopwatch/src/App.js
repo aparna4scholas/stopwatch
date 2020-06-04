36c06f7100d838c2b7427d385901e3b3fa6f4462
aparna4scholas <aparna4scholas@gmail.com>
2020-06-04 14:45:07 -0500

    added app.css

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import Stopwatch from './components/Stopwatch.js';
 
 function App() {
   return (
--- stopwatch/src/components/Stopwatch.js
+++ stopwatch/src/components/Stopwatch.js
@@ -1,33 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
 
  class Stopwatch extends Component {
      constructor(props){
          super(props);
-         this.state = {timer:0,start:false}
+         this.state = {timer:0,start: false}
          this.start = this.start.bind(this);
          this.reset = this.reset.bind(this);
          this.pause = this.pause.bind(this);
 
      }
-     start(){
-         
+     start()
+     {
+         if(!this.state.start)
+         {
+            this.setState({ start: true});
+            this.toreset=setInterval(()=>{
+                this.setState({ timer: this.state.timer + 1})},1000);
+         }
      }
-    render() {
+         
+     
+
+     reset()
+     {
+         this.setState({ start: false,timer: 0});
+         clearInterval(this.toreset);
+     }
+     pause()
+     {
+        if(this.state.start == true)
+        {
+            clearInterval(this.toreset);
+            this.setState({ start: false});
+        } else
+        {
+            if(this.state.timer!==0)
+            {
+                this.start();
+            }
+        }
+     }
+    render() 
+    {
         return (
             <div>
-                <h1></h1>
-                <button onClick={this.start} >
+                <h1> {this.state.timer} </h1>
+                <button className="b1" onClick={this.start} >
                     Start
                 </button>
-                <button onClick={this.pause} >
+                <button className="b2" onClick={this.pause} >
                     Pause
                 </button>
-                <button onClick={this.reset} >
+                <button className="b3" onClick={this.reset} >
                     Reset
                 </button>
 
             </div>
-        )
+        );
     }
-}
+
+ }
 export default Stopwatch;
